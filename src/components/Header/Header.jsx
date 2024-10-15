2718aca998087693273a826e2ed8e1e91a734bf3
Priyadharshan <s.priyadharshan.official@gmail.com>
2024-10-15 22:23:17 +0530

    Progress as of 15/10/24

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,15 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='app__header'>
-      <div class="left">
-        <i class="fas fa-search"></i>
+      <div className="left">
+        <i className="fas fa-search"></i>
       </div>
-      <div class="right">
-        <i class="fas fa-bell"></i>
-        <i class="fas fa-user"></i>
+      <div className="right">
+        <i className="fas fa-bell"></i>
+        <i className="fas fa-user"></i>
         <div className="divider"></div>   
-        <img src="https://via.placeholder.com/30" alt="Profile Image" class="profile-pic" />
-        <img src="https://via.placeholder.com/30" alt="Flag" class="flag-pic" />
+        <img src="https://via.placeholder.com/30" alt="Profile Image" className="profile-pic" />
+        <img src="https://via.placeholder.com/30" alt="Flag" className="flag-pic" />
       </div>
     </div>
   )
